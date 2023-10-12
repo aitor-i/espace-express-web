49d8162cb28b6996494b2b95cb71330615b54ffa
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-12 16:00:36 +0200

    ADD - Next flights

--- src/components/Layout/NextFlightPage/NextFlightPage.tsx
+++ src/components/Layout/NextFlightPage/NextFlightPage.tsx
@@ -1,13 +1,36 @@
+import React, { cache, use } from "react";
+
 import Navigation from "@/components/Navigation/Navigation";
-import React from "react";
+import { LAUNCHES_ENDPOINT } from "@/domain/endpoints/spaceEndpoints";
+import { Launches } from "@/domain/Models/LaunchesModels/LaunchesInterfaces";
+
+const fetchLaunches = cache(() => {
+  const endpoint = new URL(LAUNCHES_ENDPOINT);
+
+  return fetch(endpoint).then((res) => res.json());
+});
 
 export const NextFlightPage = () => {
+  const launchesFromApi = use(fetchLaunches()) as Launches;
+  const dateShortedLaunches = launchesFromApi.sort(
+    (a, b) =>
+      new Date(b.date_local).getTime() - new Date(a.date_local).getTime()
+  );
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="absolute top-4 right-14">
         <Navigation />
       </div>
       <h1 className="text-4xl self-start">Next Flights</h1>
+      <div>
+        {launchesFromApi &&
+          dateShortedLaunches.map((launch) => (
+            <p key={launch.flight_number}>
+              Crew: {JSON.stringify(launch.date_local)} for flight:{" "}
+              {launch.name}
+            </p>
+          ))}
+      </div>
     </main>
   );
 };
