ea35987877fde9bfbfb75ec7888999d94397790c
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-12 19:35:43 +0200

    ADD - Server fetching function

---
+++ src/hooks/serverFetching.ts
@@ -0,0 +1,7 @@
+import { cache } from "react";
+
+export const serverFetching = cache((url: string) => {
+  const endpoint = new URL(url);
+
+  return fetch(endpoint).then((res) => res.json());
+});
