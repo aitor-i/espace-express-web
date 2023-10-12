4dff8f13624255133fb296985aabb8f07a99a6ba
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-12 17:33:38 +0200

    ADD - Launch mapper

---
+++ src/application/mappers/launchMapper/launchMapper.ts
@@ -0,0 +1,43 @@
+import { Crew } from "@/domain/Models/LaunchesModels/CrewInterfaces";
+import { Launches } from "@/domain/Models/LaunchesModels/LaunchesInterfaces";
+import {
+  Rocket,
+  Rockets,
+} from "@/domain/Models/LaunchesModels/RocketInterfaces";
+
+interface FormattedLaunch {
+  crew: Crew[];
+  date: Date;
+  name: string;
+  rocket: Rocket;
+  number: number;
+}
+
+export function launchMapper(
+  launches: Launches,
+  rockets: Rockets,
+  crewList: Crew[]
+): FormattedLaunch[] {
+  const formattedLaunches = launches.map((launch) => {
+    const date = new Date(launch.date_local);
+    date.setFullYear(date.getFullYear() + 1);
+
+    const rocket = rockets.find((rocket) => rocket.id === launch.rocket);
+    const crew = launch.crew.map((crewMember) => {
+      return crewList.find((crew) => crew.id == crewMember);
+    });
+
+    const formattedLaunch = {
+      date,
+      name: launch.name,
+      rocket,
+      number: launch.flight_number,
+      crew,
+    } as FormattedLaunch;
+    return formattedLaunch;
+  });
+
+  formattedLaunches.sort((a, b) => a.date.getTime() - b.date.getTime());
+
+  return formattedLaunches as FormattedLaunch[];
+}
