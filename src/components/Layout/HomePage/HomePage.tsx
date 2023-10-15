c30399a11c80d4a93858940f70f9855476cdd2c9
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-15 15:36:11 +0200

    ADD - Home page bg image

--- src/components/Layout/HomePage/HomePage.tsx
+++ src/components/Layout/HomePage/HomePage.tsx
@@ -1,13 +1,23 @@
 import Navigation from "@/components/Navigation/Navigation";
-import React from "react";
+import Image from "next/image";
+import React, { Fragment } from "react";
+
+import Earth from "@/../public/img/earth-1.jpeg";
 
 export const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="absolute top-4 right-14">
-        <Navigation />
+    <div className="h-screen overflow-y-hidden">
+      <div className="flex h-full overflow-hidden flex-col items-center justify-between p-24 overflow-y-hidden glassEffect ">
+        <div className="absolute top-4 right-14">
+          <Navigation />
+        </div>
+        <h1 className="primary-color text-8xl z-10 font-bold">Space Express</h1>
       </div>
-      <h1 className="primary-color text-7xl">Space Express</h1>
-    </main>
+      <Image
+        className="absolute -z-10 h-screen w-screen top-40 backgroundImgFit"
+        src={Earth}
+        alt=""
+      />
+    </div>
   );
 };
