048bb4011fd4a6951eb3863f6ab837e5533ac7fe
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-15 12:20:09 +0200

    ADD - Capsules endpoint

--- src/domain/endpoints/spaceEndpoints.ts
+++ src/domain/endpoints/spaceEndpoints.ts
@@ -1,3 +1,4 @@
 export const LAUNCHES_ENDPOINT = "https://api.spacexdata.com/v4/launches";
 export const ROCKETS_ENDPOINT = "https://api.spacexdata.com/v4/rockets";
 export const CREW_ENDPOINT = "https://api.spacexdata.com/v4/crew";
+export const CAPSULES_ENDPOINT = "https://api.spacexdata.com/v4/capsules";
