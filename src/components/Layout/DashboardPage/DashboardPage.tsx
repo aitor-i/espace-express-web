272e2bfaaacf1cecac3202140ecf4c80f5d721dd
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-19 06:49:21 +0200

    ADD - Token validation & redirection

--- src/components/Layout/DashboardPage/DashboardPage.tsx
+++ src/components/Layout/DashboardPage/DashboardPage.tsx
@@ -1,14 +1,48 @@
 import Navigation from "@/components/Navigation/Navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import BgImage from "@/../public/img/mart-2.jpeg";
+import { IFetchParams, useFetch } from "@/hooks/useFetch";
+import { redirect } from "next/navigation";
 interface Props {
   username: string;
 }
+
+interface ValidateResponse {
+  message: string;
+  isValid: boolean;
+}
 export default function DashboardPage({ username }: Props) {
+  const { fetcher, fetchingStatus, response, rowResponse } =
+    useFetch<ValidateResponse>();
+
+  if (fetchingStatus === "succeeded" && response?.isValid === false) {
+    logOut();
+  }
+
+  useEffect(() => {
+    validateToken();
+  }, []);
+
+  async function validateToken() {
+    const token = window.localStorage.getItem("token");
+    const body = { token };
+    const validateSettings: IFetchParams = {
+      method: "POST",
+      url: `http://localhost:4000/api/space-express/auth/validate-token`,
+      body,
+      headers: { "Content-Type": "application/json" },
+    };
+    await fetcher(validateSettings);
+  }
+
+  function logOut() {
+    window.localStorage.setItem("token", "");
+    redirect("/login");
+  }
   return (
     <main>
-      <h2>{username}</h2>
+      <h2>{username.replace("%20", " ")}</h2>
       <div className="absolute top-4 right-14">
         <Navigation />
       </div>
