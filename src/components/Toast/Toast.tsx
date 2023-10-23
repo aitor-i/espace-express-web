9a90ce2fa9d2d2e843d7bf7bdec6a5510fac6893
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-23 06:55:31 +0200

    ADD - ClassName prop

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -2,8 +2,13 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
-export default function Toast({ children }: Props) {
-  return <span className="toast glassEffect warn rounded-md">{children}</span>;
+export default function Toast({ children, className }: Props) {
+  return (
+    <span className={` toast glassEffect warn rounded-md  ${className}`}>
+      {children}
+    </span>
+  );
 }
