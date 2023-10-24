72ef56959eb64e365548178001d01c3df25e991f
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-24 06:33:27 +0200

    TEST - should return invalid and provide a message for a password without an uppercase letter

--- src/application/passwordValidation/passwordValidation.test.ts
+++ src/application/passwordValidation/passwordValidation.test.ts
@@ -18,4 +18,12 @@ describe("password validation", () => {
       "Password is not strong enough. It must contain at least one uppercase letter, one number, and be at least 8 characters long."
     );
   });
+
+  it("should return invalid and provide a message for a password without an uppercase letter", () => {
+    const result = passwordValidation("12345678");
+    expect(result.valid).toBe(false);
+    expect(result.message).toBe(
+      "Password is not strong enough. It must contain at least one uppercase letter, one number, and be at least 8 characters long."
+    );
+  });
 });
