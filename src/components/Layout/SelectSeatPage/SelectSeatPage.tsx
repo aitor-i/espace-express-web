c1da67d6fbc857a1c78dd91182601022734816b2
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-11-10 16:57:27 +0100

    FIX -Token

--- src/components/Layout/SelectSeatPage/SelectSeatPage.tsx
+++ src/components/Layout/SelectSeatPage/SelectSeatPage.tsx
@@ -3,6 +3,9 @@
 import { IFetchParams, useFetch } from "@/hooks/useFetch";
 import { useEffect, useState } from "react";
 import { ReserveSeatForm } from "./ReserveSeatForm/ReserveSeatForm";
+import Toast from "@/components/Toast/Toast";
+import Navigation from "@/components/Navigation/Navigation";
+import Link from "next/link";
 interface Props {
   flightId: string;
 }
@@ -19,6 +22,9 @@ interface SeatsResponse {
   seats: Seat[];
 }
 export const SelectSeatPage = ({ flightId }: Props) => {
+  const [token, setToken] = useState<string>();
+  console.log(token);
+
   const { fetcher, fetchingStatus, response, rowResponse } =
     useFetch<SeatsResponse>();
   const [selectedNumber, setSelectedNumber] = useState<number>();
@@ -29,7 +35,6 @@ export const SelectSeatPage = ({ flightId }: Props) => {
 
   const baseUrl = process.env.NEXT_PUBLIC_AUTH_API_BASE_URL;
   const header = new Headers();
-  const token = window.localStorage.getItem("token");
   header.append("token", token!);
 
   const fetchParams = {
@@ -38,44 +43,65 @@ export const SelectSeatPage = ({ flightId }: Props) => {
     headers: { "Content-Type": "application/json", token: token, id: flightId },
   } as IFetchParams;
 
-  useEffect(() => {
+  const fetchSeatsAvailability = () => {
     fetcher(fetchParams);
-  }, []);
+    setSelectedNumber(undefined);
+  };
+
+  useEffect(() => {
+    setToken(window.localStorage.getItem("token") ?? "  ");
+    if (token) fetchSeatsAvailability();
+  }, [token]);
 
   return (
     <main>
-      <h2>Select your seat</h2>
-      <p>Flight id: {flightId}</p>
-      <div className=" flex justify-between p-24">
-        <section className="flex gap-3 flex-wrap w-1/3">
-          {fetchingStatus === "succeeded" && rowResponse?.ok
-            ? response?.seats.map((seat) =>
-                seat.free ? (
-                  <span
-                    key={seat.seatNumber}
-                    onClick={() => selectNumberHandler(seat.seatNumber)}
-                    className="p-4 bg-green-400 w-20 text-center clickable text-white rounded-md "
-                  >
-                    {seat.seatNumber}
-                  </span>
-                ) : (
-                  <span
-                    key={seat.seatNumber}
-                    className="p-4 bg-red-400 w-20 text-center text-white rounded-md"
-                  >
-                    {seat.seatNumber}
-                  </span>
-                )
-              )
-            : null}
-        </section>
-        {selectedNumber ? (
-          <ReserveSeatForm
-            flightId={flightId}
-            selectedNumber={selectedNumber}
-          />
-        ) : null}
-      </div>
+      {response?.message ? <Toast>{response.message}</Toast> : null}
+      <span className="absolute top-4 right-4">
+        <Navigation />
+      </span>
+      {rowResponse?.status !== 403 ? (
+        <>
+          {" "}
+          <h2>Select your seat</h2>
+          <p>Flight id: {flightId}</p>
+          <div className=" flex justify-between p-24">
+            <section className="flex gap-3 flex-wrap w-1/3">
+              {fetchingStatus === "succeeded" && rowResponse?.ok
+                ? response?.seats.map((seat) =>
+                    seat.free ? (
+                      <span
+                        key={seat.seatNumber}
+                        onClick={() => selectNumberHandler(seat.seatNumber)}
+                        className="p-4 bg-green-400 w-20 text-center clickable text-white rounded-md "
+                      >
+                        {seat.seatNumber}
+                      </span>
+                    ) : (
+                      <span
+                        key={seat.seatNumber}
+                        className="p-4 bg-red-400 w-20 text-center text-white rounded-md"
+                      >
+                        {seat.seatNumber}
+                      </span>
+                    )
+                  )
+                : null}
+            </section>
+            {selectedNumber ? (
+              <ReserveSeatForm
+                flightId={flightId}
+                selectedNumber={selectedNumber}
+                onReservationSuccess={fetchSeatsAvailability}
+              />
+            ) : null}
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>You need to be logged</h2>
+          <Link href="/login"> Login</Link>
+        </>
+      )}
     </main>
   );
 };
